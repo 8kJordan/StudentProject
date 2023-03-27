9b1e7ead3eca2a2e3d3b4debb1fb4b66cea75632
Jordan Sanchez <89486272+8kJordan@users.noreply.github.com>
2023-03-27 00:39:09 -0400

    Add files via upload
    Test class that initiates the program

---
+++ TestClassComposite.java
@@ -0,0 +1,9 @@
+package examTwoComposite;
+
+import java.util.*;
+
+public class TestClassComposite {
+    public static void main(String[] args) {
+        Methods.menu(new ArrayList<Student>());
+    }//end main
+}//end TestClassComposite
